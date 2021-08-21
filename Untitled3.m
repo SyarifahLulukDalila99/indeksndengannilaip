622667640e39f54e62abee59b3abbfe9a2b99418
SyarifahLulukDalila99 <87511674+SyarifahLulukDalila99@users.noreply.github.com>
2021-08-21 13:47:19 +0800

    Add files via upload
    menentukan nilai indeks n dengan nilai p yang diketahui

---
+++ Untitled3.m
@@ -0,0 +1,7 @@
+n1=[2,3,4,5,6]; %definisikan indeks sampel n
+n2=0;
+p1=[1,1,1,1,1]; %tentukan nilai p terhadap n
+p2=0;
+stem(n1,p1); 
+xlabel('n'); % menambahkan teks disamping sumbu x berupa "n"
+ylabel('p[n]'); % menambahakan teks disamping sumbu y berupa "p[n]"
